7a486e53faede2a7e121431731387027a588c69b
tycoder1 <tyty122704@gmail.com>
2020-07-27 15:42:39 +0000

    Changed indentations for errors and added pseudo code

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 #make function to recive birthday information
 #get user input for
-def birthday_function(a,b,c,d):
-    a = input('enter birth month ')
-b = input(' enter birthday ')
+def birthday_function():
+    birth_m = input('Enter birth month ')
+    birth_d = int(input('Enter birth day '))
+    user_y = input('Enter birth year ')
+    user_n = input(' Enter your name ')
+    month_days = [0,31,59,90,120,151,181,212,243,274,304,]
+    Day = (month_days+ birth_d) - 59 
+
+birthday_function()
