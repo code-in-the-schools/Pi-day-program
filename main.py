af83081d85886e23b74723afac04235702416c3f
tycoder1 <>
2021-05-16 18:05:55 +0000

    Fully defined function implemented

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import datetime
-x = datetime.datetime.now()
-print(x)
 
-def Calculate_Bdays(x,y):
-    
+def Calculate_Bdays(x): #This function accounts for all the birthdays in the list birtdays and finds how many days until then
+    x = datetime.datetime.now()
+    if Output_Bdays == Names(0):
+        Days = x - Birthdays(0)
+        print(Names(0)+ "birthdy is" + Birthdays(0)+ "It's in"+ Days)
 
 Names = []
 Birthdays = []
+x = datetime.datetime.now()
 Start_prompt = input("Would you like to add a birthday? ")
 if Start_prompt =="yes": #This will take all nesscary input from the user to store someones birthday
     Collect_names = input("Enter their name ")
@@ -16,13 +18,15 @@ if Start_prompt =="yes": #This will take all nesscary input from the user to sto
     z=input("Would you like to add another birthday? ")
     while z == "yes":
         Collect_names = input("Enter their name ")
-    Names.append(Collect_names)
-    Collect_Bdays = input("Enter their birthday(m/d/yyyy) ")
-    Birthdays.append(Collect_Bdays)
+        Names.append(Collect_names)
+        Collect_Bdays = input("Enter their birthday(m/d/yyyy) ")
+        Birthdays.append(Collect_Bdays)
     if z == "no":
-        Output_Bdays = input("would you like to know someones birthday? ")
-        if Birthdays.len(0):#This is to make sure there is a birthday to return
+        Second_prompt = input("would you like to know someones birthday? ")
+        if len(Birthdays) == 0:#This is to make sure there is a birthday to return
             print("You need to add a birthday first")
         else:
             print(Names)
-            input("Whose birthday would you like to know? ")
+            Output_Bdays=input("Whose birthday would you like to know? ")
+            Calculate_Bdays(x)
+
