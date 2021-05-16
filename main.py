09c0bf9a183b85a02be4ce885ca89474d8aa8395
tycoder1 <>
2021-05-16 17:39:54 +0000

    Now takes all input from user to add someone's birthday.

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-#make function to recive birthday information
-#get user input for
-def birthday_function():
-    birth_m = input('Enter birth month ')
-    birth_d = int(input('Enter birth day '))
-    user_y = input('Enter birth year ')
-    user_n = input(' Enter your name ')
-    month_days = [0,31,59,90,120,151,181,212,243,274,304,]
-    Day = (month_days+ birth_d) - 59 
+import datetime
+x = datetime.datetime.now()
+print(x)
 
-birthday_function()
+def Calculate_Bdays(x,y):
+    
+
+Names = []
+Birthdays = []
+Start_prompt = input("Would you like to add a birthday? ")
+if Start_prompt =="yes": #This will take all nesscary input from the user to store someones birthday
+    Collect_names = input("Enter their name ")
+    Names.append(Collect_names)
+    Collect_Bdays = input("Enter their birthday(yyyy/m/d) ")
+    Birthdays.append(Collect_Bdays)
+    z=input("Would you like to add another birthday? ")
+    while z == "yes":
+        Collect_names = input("Enter their name ")
+    Names.append(Collect_names)
+    Collect_Bdays = input("Enter their birthday(m/d/yyyy) ")
+    Birthdays.append(Collect_Bdays)
+    if z == "no":
+        Output_Bdays = input("would you like to know someones birthday? ")
+        if Birthdays.len(0):#This is to make sure there is a birthday to return
+            print("You need to add a birthday first")
+        else:
+            print(Names)
+            input("Whose birthday would you like to know? ")
